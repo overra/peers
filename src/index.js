b1389140551982ede9da17b767f6cfc41c90995a
Adam Snodgras <overra@gmail.com>
2018-11-21 19:34:09 -0600

    remove extra code

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import { html, render } from "htm/preact/standalone";
 import Layout from "../components/layout";
 import GraphQL from "../components/gql";
 
-const App = () =>
+render(
   html`
     <${GraphQL} url="https://1prlzv487.sse.codesandbox.io">
       <${Layout} />
     <//>
-  `;
-render(html`<${App} />`, document.getElementById("app"));
+  `,
+  document.getElementById("app")
+);
